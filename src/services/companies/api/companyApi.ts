e083cb70a92bda29243ce2b96e509dae480c8e67
Park, Su Eun <park_sueun@naver.com>
2026-03-17 22:04:52 +0800

    feature: company api client for get, update, delete requests

---
+++ src/services/companies/api/companyApi.ts
@@ -0,0 +1,25 @@
+import { apiClient } from "@/lib/apiClient";
+import { CompanyCreateRequest } from "../components/CompanyForm";
+
+export const createCompany = (data: CompanyCreateRequest) => {
+  return apiClient.post("/api/admin/companies", data);
+};
+
+export const getCompanies = async () => {
+  const res = await apiClient.get("/api/companies")
+  console.log(res.data.content)
+  return res.data.content
+}
+
+export const getCompany = async (id: number) => {
+  const res = await apiClient.get(`/api/companies/${id}`)
+  return res.data
+}
+
+export const updateCompany = async (id: number, data: any) => {
+  return apiClient.put(`/api/admin/companies/${id}`, data)
+}
+
+export const deleteCompany = async (id: number) => {
+  return apiClient.delete(`/api/admin/companies/${id}`)
+}
