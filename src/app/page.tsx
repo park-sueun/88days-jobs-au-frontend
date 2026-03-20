19561db5379a052330c440d8b044ebd5e7b9890b
Park, Su Eun <park_sueun@naver.com>
2026-03-20 17:57:05 +0800

    Header, PopularJobs Section 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
-import Header from "@/domains/home/components/Header";
 import Hero from "@/domains/home/components/Hero";
-import PopularJobs from "@/domains/home/components/PopularJobs";
 import Footer from "@/domains/home/components/Footer";
+import FeaturedJobs from "@/domains/home/components/FeaturedJobs";
+import Navbar from "@/shared/components/layout/Navbar";
+import TopCompanies from "@/domains/home/components/TopCompanies";
 
 export default function HomePage() {
   return (
     <main className="bg-gray-50 min-h-screen">
-      <Header />
+      <Navbar />
       <Hero />
-      <PopularJobs />
+      <FeaturedJobs />
+      <TopCompanies />
       <Footer />
     </main>
   );
